7140a464956c1d388ccca085afd3868ca2b29f80
Maansi <maansigarg06@gmail.com>
2023-11-25 12:11:28 +0530

    create team list

--- lib/screens/teamDetail.dart
+++ lib/screens/teamDetail.dart
@@ -11,53 +11,105 @@ class _t_detailState extends State<t_detail> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.utc(2021, 11, 29); // Set the initial focus day
   DateTime? _selectedDay;
+  Future<void>? _futureData;
+  void initState() {
+    super.initState();
+    // _futureData =api();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar:AppBar(title: Text("Team A"),),
-        body: Container(
-          child: Column(
-            children:[
-              Table(
-              border: TableBorder.all(),
-                children: [
-                TableRow(
+        body: SafeArea(
+          child: Container(
+            child: Column(
+              children:[
+                Text("Leader - Sir"),
+                SizedBox(height: 10,),
+                Table(
+                  columnWidths: {
+                    0: FixedColumnWidth(60.0),
+                    1: FixedColumnWidth(90.0),
+                    2: FixedColumnWidth(100.0),
+                    3: FixedColumnWidth(80.0),
+                  },
+                border: TableBorder.all(),
                   children: [
-                    TableCell(
-                      child: Center(child: Icon(Icons.check_circle_outline)),
-                    ),
-                    TableCell(
-                      child: Center(child: Text('Member')),
-                    ),
-                    TableCell(
-                      child: Center(child: Text('Task')),
-                    ),
-                    TableCell(
-                      child: Center(child: Text('Deadline')),
-                    ),
-                  ],
-                ),
                   TableRow(
-                    children:[
+                    children: [
                       TableCell(
-                        child: Center(child: Icon(Icons.check_circle_outline,color: Colors.red,)),
+                        child: Center(child: Text("Status")),
                       ),
                       TableCell(
-                        child: Center(child: Text('Maansi')),
+                        child: Center(child: Text('Member')),
                       ),
                       TableCell(
-                        child: Center(child: Text('Create App')),
+                        child: Center(child: Text('Task')),
                       ),
                       TableCell(
-                        child: Center(child:Text("29-11-23") ),
+                        child: Center(child: Text('Deadline')),
                       ),
-                    ]
-                  )
-              ],
-            ),
-          ],
+                    ],
+                  ),],),
+                    FutureBuilder<void>(
+                      future: _futureData,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return Center(
+                          child: CircularProgressIndicator(
+                            color:Colors.white,
+                            ),
+                            );
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          return Expanded(
+                            child: ListView.builder(
+                            itemCount:3,
+                              itemBuilder: (context,index) {
+                                return ListTile(
+                                  title: Table(
+                                    columnWidths: {
+                                      0: FixedColumnWidth(60.0),
+                                      1: FixedColumnWidth(90.0),
+                                      2: FixedColumnWidth(100.0),
+                                      3: FixedColumnWidth(80.0),
+                                    },
+                                    border: TableBorder.all(),
+                                    children: [
+                                      TableRow(
+                                          children: [
+                                            TableCell(
+                                              child: Center(child: Icon(
+                                                Icons.check_circle_outline,
+                                                color: Colors.red,)),
+                                            ),
+                                            TableCell(
+                                              child: Center(child: Text('Maansi')),
+                                            ),
+                                            TableCell(
+                                              child: Center(
+                                                  child: Text('Create App')),
+                                            ),
+                                            TableCell(
+                                              child: Center(
+                                                  child: Text("29-11-23")),
+                                            ),
+                                          ]
+                                      )
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                          }
+                      },
+                    ),
+            ],
+          ),
+                ),
         ),
-      ),
     );
   }
 }
