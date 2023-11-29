41138b902539059062edd3bcdc50892b73fb9985
Ananya Singhal <singhalananya67@gmail.com>
2023-11-30 01:48:14 +0530

    tasks done

--- lib/home_page/tasks.dart
+++ lib/home_page/tasks.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:brl_task4/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -10,15 +11,18 @@ class TaskContainer extends StatefulWidget {
 
 class _TaskContainerState extends State<TaskContainer> {
   Future<void>? _futureData;
+  Future<void>? _futureData2;
   List<dynamic>? compTasks;
+  List<dynamic>? incompTasks;
   String? task;
 
   void initState() {
     super.initState();
-    _futureData = showTeamAPI();
+    _futureData = incompTaskAPI();
+    _futureData2 = compTaskAPI();
   }
 
-  Future<void> showTeamAPI() async {
+  Future<void> incompTaskAPI() async {
     dynamic storedValue = await secureStorage.readSecureData(key);
 
     final String apiUrl =
@@ -33,7 +37,31 @@ class _TaskContainerState extends State<TaskContainer> {
 
     if (response.statusCode == 200) {
       setState(() {
-        compTasks = jsonDecode(response.body)['incompleteTasks'];
+        incompTasks = jsonDecode(response.body)['incompleteTasks'];
+        print(incompTasks);
+      });
+    } else {
+      print(' ${response.statusCode}');
+      print('Error Message: ${response.body}');
+    }
+  }
+
+  Future<void> compTaskAPI() async {
+    dynamic storedValue = await secureStorage.readSecureData(key);
+
+    final String apiUrl =
+        'http://ec2-3-7-70-25.ap-south-1.compute.amazonaws.com:8006/team/completedTasks';
+
+    final response = await http.get(
+      Uri.parse(apiUrl),
+      headers: <String, String>{
+        'Authorization': storedValue,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        compTasks = jsonDecode(response.body)['completedTasks'];
         print(compTasks);
       });
     } else {
@@ -45,24 +73,15 @@ class _TaskContainerState extends State<TaskContainer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black,
-            width: 1,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-        ),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Image.asset(
               'lib/assets/login.png',
-              width: 100,
-              height: 100,
+              width: MediaQuery.of(context).size.width * 0.15,
+              height: MediaQuery.of(context).size.height * 0.15,
             ),
-            SizedBox(height: 16.0),
             Text(
               'Tasks',
               style: TextStyle(
@@ -70,57 +89,124 @@ class _TaskContainerState extends State<TaskContainer> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 16.0),
-            FutureBuilder<void>(
-              future: _futureData,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      color: Colors.purple,
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  if (compTasks == null) {
-                    return Text("No tasks");
-                  }
-                  return Container(
-                    height: 300,
-                    child: ListView.builder(
-                      itemCount: compTasks!.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: TextButton(
-                            onPressed: () {
-                              _showAlert(compTasks![index]['description'],compTasks![index]['deadline']);
-                            },
-                            child: Container(
-                              width: MediaQuery.of(context).size.width,
-                              height: MediaQuery.of(context).size.height * 0.1,
-                              color: Color.fromARGB(255, 147, 78, 158),
-                              child: Row(
-                                children: [
-                                  SizedBox(width: 20,),
-                                  Text(
-                                    compTasks![index]['description'],
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                }
-              },
-            ),
+            SizedBox(height: 10),
+            Text('Incomplete Tasks:- ',style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            showIncompletetask(),
+            SizedBox(height: 20,),
+            Text('Completed Tasks:- ',style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+            showCompletedtask(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget showIncompletetask() {
+    return FutureBuilder<void>(
+      future: _futureData,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.purple,
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          if (incompTasks == null || incompTasks!.isEmpty) {
+            return Text("No incomplete tasks");
+          }
+          return Container(
+            height: 300,
+            child: ListView.builder(
+              itemCount: incompTasks!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: TextButton(
+                    onPressed: () {
+                      _showAlert(incompTasks![index]['description'],
+                          incompTasks![index]['deadline']);
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      color: Color.fromARGB(255, 147, 78, 158),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            incompTasks![index]['description'] ?? "No tasks",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget showCompletedtask() {
+    return FutureBuilder<void>(
+      future: _futureData2,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.purple,
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          if (compTasks == null || compTasks!.isEmpty) {
+            return Text("No tasks");
+          }
+          return Container(
+            height: 300,
+            child: ListView.builder(
+              itemCount: compTasks!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: TextButton(
+                    onPressed: () {
+                      _showAlert(compTasks![index]['description'],
+                          compTasks![index]['deadline']);
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      color: Color.fromARGB(255, 147, 78, 158),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            compTasks![index]['description'] ?? "No tasks",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      },
     );
   }
 
@@ -143,5 +229,4 @@ class _TaskContainerState extends State<TaskContainer> {
       },
     );
   }
-
 }
