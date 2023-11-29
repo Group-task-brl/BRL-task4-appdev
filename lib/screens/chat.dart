a4bd7c47b3190ab35f0afff6c3249ae06d020b3f
Euclid Stellar <euclidstellar@gmail.com>
2023-11-29 23:46:57 +0530

    chat Ui done

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,259 +1,5 @@
-
-// import 'package:flutter/material.dart';
-// import 'package:socket_io_client/socket_io_client.dart' as IO;
-
-// class ChatScreen extends StatefulWidget {
-//   ChatScreen(String? leaderEmail);
-
-//   @override
-//   _ChatScreenState createState() => _ChatScreenState( );
-// }
-
-// class _ChatScreenState extends State<ChatScreen> {
-//   final TextEditingController _controller = TextEditingController();
-//   final List<Map<String, dynamic>> _messages = [];
-//   late IO.Socket socket;
-//   late String myUsername; // Add this line
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     myUsername = 'test'; // Set your desired username here
-//     initSocket();
-//   }
-
-//   void initSocket() {
-//     // Replace 'http://localhost:4000' with the actual IP address or hostname of your Socket.IO server
-//     socket = IO.io('http://3.7.70.25:8006', <String, dynamic>{
-//       'transports': ['websocket'],
-//       'autoConnect': false,
-//     });
-//     socket.connect();
-//     socket.onConnect((_) {
-//       print('Connected: ${socket.id}');
-//     });
-//     socket.on('message', (data) {
-//       setState(() {
-//         if (data['username'] != null) {
-//         _messages.add({
-//           'message': data['message'],
-//           'username': data['username'],
-//         });
-//       }
-//        // _messages.add({'message': data['message'], 'username': data['username'] , 'teamId' : data['mohit ke chuche']  });
-//       });
-//     });
-//   }
-//   void joinTeam(String teamId) {
-//     socket.emit('join', {'teamId': teamId, 'username': myUsername});
-//   }
-
-//   void _sendMessage() {
-//     if (_controller.text.isNotEmpty) {
-//       socket.emit('message', {'message': _controller.text, 'username': myUsername});
-//       setState(() {
-//         _messages.add({'message': _controller.text, 'username': myUsername});
-//       });
-//       _controller.clear();
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Flutter Chat App'),
-//       ),
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: _messages.length,
-//               itemBuilder: (context, index) {
-//                 final message = _messages[index]['message'];
-//                 final username = _messages[index]['username'];
-
-//                 return ListTile(
-//                   title: Container(
-//                     padding: EdgeInsets.all(8.0),
-//                     decoration: BoxDecoration(
-//                       color: username == myUsername ? Colors.blue : Colors.grey,
-//                       borderRadius: BorderRadius.circular(8.0),
-//                     ),
-//                     child: Text(
-//                       '$username: $message',
-//                       style: TextStyle(color: Colors.white),
-//                     ),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Row(
-//               children: [
-//                 Expanded(
-//                   child: TextField(
-//                     controller: _controller,
-//                     decoration: InputDecoration(
-//                       hintText: 'Enter your message...',
-//                     ),
-//                   ),
-//                 ),
-//                 IconButton(
-//                   icon: Icon(Icons.send),
-//                   onPressed: _sendMessage,
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     socket.disconnect();
-//     super.dispose();
-//   }
-// }
-
-
-// import 'package:flutter/material.dart';
-// import 'package:socket_io_client/socket_io_client.dart' as IO;
-
-// class ChatScreen extends StatefulWidget {
-//   final String leaderEmail;
-
-//   ChatScreen(this.leaderEmail);
-
-//   @override
-//   _ChatScreenState createState() => _ChatScreenState();
-// }
-
-// class _ChatScreenState extends State<ChatScreen> {
-//   final TextEditingController _controller = TextEditingController();
-//   final List<Map<String, dynamic>> _messages = [];
-//   late IO.Socket socket;
-//   late String myUsername; 
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     myUsername = widget.leaderEmail; 
-//     initSocket();
-//   }
-
-//   void initSocket() {
-//     // Replace 'http://localhost:4000' with the actual IP address or hostname of your Socket.IO server
-//     socket = IO.io('http://3.7.70.25:8006', <String, dynamic>{
-//       'transports': ['websocket'],
-//       'autoConnect': false,
-//     });
-//     socket.connect();
-//     socket.onConnect((_) {
-//       print('Connected: ${socket.id}');
-//     });
-//     socket.on('message', (data) {
-//       setState(() {
-//         if (data['username'] != null) {
-//           _messages.add({
-//             'message': data['message'],
-//             'username': data['username'],
-//           });
-//         }
-//       });
-//     });
-//   }
-
-//   void joinTeam(String teamId) {
-//     socket.emit('join', {'teamId': teamId, 'username': myUsername});
-//   }
-
-//   void _sendMessage() {
-//     if (_controller.text.isNotEmpty) {
-//       socket.emit('message', {'message': _controller.text, 'username': myUsername});
-//       setState(() {
-//         _messages.add({'message': _controller.text, 'username': myUsername});
-//       });
-//       _controller.clear();
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Flutter Chat App'),
-//       ),
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: _messages.length,
-//               itemBuilder: (context, index) {
-//                 final message = _messages[index]['message'];
-//                 final username = _messages[index]['username'];
-
-//                 return ListTile(
-//                   title: Container(
-//                     padding: EdgeInsets.all(8.0),
-//                     decoration: BoxDecoration(
-//                       color: username == myUsername ? Colors.blue : Colors.grey,
-//                       borderRadius: BorderRadius.circular(8.0),
-//                     ),
-//                     child: Text(
-//                       '$username: $message',
-//                       style: TextStyle(color: Colors.white),
-//                     ),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Row(
-//               children: [
-//                 Expanded(
-//                   child: TextField(
-//                     controller: _controller,
-//                     decoration: InputDecoration(
-//                       hintText: 'Enter your message...',
-//                     ),
-//                   ),
-//                 ),
-//                 IconButton(
-//                   icon: Icon(Icons.send),
-//                   onPressed: _sendMessage,
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     socket.disconnect();
-//     super.dispose();
-//   }
-// }
-
-
-
-
-// chat 
-
-
-
 import 'dart:convert';
 import 'dart:math';
-
 import 'package:brl_task4/screens/chatHistory.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -280,11 +26,10 @@ class _ChatScreenState extends State<ChatScreen> {
     super.initState();
     myUsername = widget.name;
     initSocket();
-    loadMessages(); 
+    loadMessages();
   }
 
-    void initSocket() {
-    // Replace 'http://localhost:4000' with the actual IP address or hostname of your Socket.IO server
+  void initSocket() {
     socket = IO.io('http://3.7.70.25:8006', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
@@ -301,7 +46,6 @@ class _ChatScreenState extends State<ChatScreen> {
             'username': data['username'],
           });
 
-        
           if (!userColors.containsKey(data['username'])) {
             userColors[data['username']] = _generateRandomColor();
           }
@@ -311,7 +55,8 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   Color _generateRandomColor() {
-    return Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0);
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+        .withOpacity(1.0);
   }
 
   void joinTeam(String teamId) {
@@ -320,8 +65,9 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void _sendMessage() {
     if (_controller.text.isNotEmpty) {
-      socket.emit('message', {'message': _controller.text, 'username': myUsername});
-      saveMessage({'message': _controller.text, 'username': myUsername}); 
+      socket.emit(
+          'message', {'message': _controller.text, 'username': myUsername});
+      saveMessage({'message': _controller.text, 'username': myUsername});
       setState(() {
         _messages.add({'message': _controller.text, 'username': myUsername});
       });
@@ -350,7 +96,8 @@ class _ChatScreenState extends State<ChatScreen> {
 
     if (messagesJson != null) {
       setState(() {
-        _messages.addAll(json.decode(messagesJson).cast<Map<String, dynamic>>());
+        _messages
+            .addAll(json.decode(messagesJson).cast<Map<String, dynamic>>());
       });
     }
   }
@@ -359,15 +106,33 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 12,
+        shadowColor: Colors.black,
+        foregroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 40, 8, 43),
         actions: [
-          IconButton(icon: Icon(Icons.chat_sharp), onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatScreenfetch() ));
-          }),
+          IconButton(
+              icon: Icon(
+                Icons.history,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ChatScreenfetch()));
+              }),
         ],
-        title: Text('Flutter Chat App'),
+        title: Text(
+          'Flutter Chat App',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
       body: Column(
         children: [
+          const SizedBox(
+            height: 5.0,
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: _messages.length,
@@ -376,12 +141,16 @@ class _ChatScreenState extends State<ChatScreen> {
                 final username = _messages[index]['username'];
 
                 return Align(
-                  alignment: username == myUsername ? Alignment.centerRight : Alignment.centerLeft,
+                  alignment: username == myUsername
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
                   child: Container(
                     padding: EdgeInsets.all(8.0),
-                    margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
                     decoration: BoxDecoration(
-                      color: userColors[username] ?? Color.fromARGB(255, 92, 92, 214),
+                      color: userColors[username] ??
+                          Color.fromARGB(255, 92, 92, 214),
                       borderRadius: BorderRadius.circular(8.0),
                     ),
                     child: Text(
@@ -398,15 +167,15 @@ class _ChatScreenState extends State<ChatScreen> {
             child: Row(
               children: [
                 Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your message...',
-                    ),
-                  ),
-                ),
+                    child: MyTextField3(
+                        hintText: 'Enter Your Message',
+                        inputType: TextInputType.name,
+                        labelText2: 'Message </>',
+                        secure1: false,
+                        capital: TextCapitalization.none,
+                        nameController1: _controller)),
                 IconButton(
-                  icon: Icon(Icons.send),
+                  icon: Icon(Icons.send_sharp),
                   onPressed: _sendMessage,
                 ),
               ],
@@ -424,5 +193,57 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
+class MyTextField3 extends StatelessWidget {
+  const MyTextField3({
+    super.key,
+    required this.hintText,
+    required this.inputType,
+    required this.labelText2,
+    required this.secure1,
+    required this.capital,
+    required this.nameController1,
+    //required this.icon
+  });
 
+  final String hintText;
+  final TextInputType inputType;
+  final String labelText2;
+  final bool secure1;
+  final TextCapitalization capital;
+  final TextEditingController nameController1;
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 10,
+      ),
+      child: TextFormField(
+        //maxLines: 5,
+        style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+        controller: nameController1,
+        keyboardType: inputType,
+        obscureText: secure1,
+        textInputAction: TextInputAction.next,
+        textCapitalization: capital,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(20),
+          hintText: hintText,
+          hintStyle: const TextStyle(color: Color.fromARGB(255, 37, 10, 38)),
+          enabledBorder: const OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Color.fromARGB(255, 37, 10, 38), width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Color.fromARGB(255, 37, 10, 38), width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          labelText: labelText2,
+          labelStyle: const TextStyle(color: Color.fromARGB(255, 37, 10, 38)),
+        ),
+      ),
+    );
+  }
+}
