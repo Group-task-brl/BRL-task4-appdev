a3d2c78087b45bec4e868f182de60a0f19c808d9
Ananya Singhal <singhalananya67@gmail.com>
2023-11-30 21:48:25 +0530

    initiate reset pass

--- lib/screens/forgotpass.dart
+++ lib/screens/forgotpass.dart
@@ -1,78 +1,93 @@
 import 'dart:convert';
 
-import 'package:brl_task4/models/storeToken.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../models/storeToken.dart';
-import '../utils/Routes.dart';
 
 class ResetPass extends StatefulWidget {
-  const ResetPass({super.key});
+  const ResetPass({Key? key}) : super(key: key);
 
   @override
   State<ResetPass> createState() => _ResetPassState();
 }
 
-final SecureStorage secureStorage = SecureStorage();
-String key = 'accessToken';
-
 class _ResetPassState extends State<ResetPass> {
-
   TextEditingController emailController = TextEditingController();
-   final _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
-  Future<void> TakePasswordAPI() async {
+  Future<String?> takeEmailAPI(String email) async {
     final String apiUrl =
         'http://ec2-3-7-70-25.ap-south-1.compute.amazonaws.com:8006/user/resetPassword';
-    final response = await http.post(Uri.parse(apiUrl),
-        body: ({
-          'email': emailController.text,
-        }));
-    print(response.body);
+    var headers = {
+      'accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    var body = jsonEncode({
+      "email": email,
+    });
+    var response =
+        await http.post(Uri.parse(apiUrl), headers: headers, body: body);
     if (response.statusCode == 200) {
-      dynamic generateResponse = jsonDecode(response.body);
-      Token.fromJson(generateResponse);
-      await secureStorage.writeSecureData(key, generateResponse);
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("${response.body}"),
-        ),
-      );
-      print('API Response: ${response.body}');
-      await Navigator.pushNamed(context, MyRoutes.LoginRoutes);
+      print('Request for OTP successful');
+      print(jsonDecode(response.body));
+      return null;
     } else {
-      print('Failed. Status Code: ${response.statusCode}');
-      print('Error: ${response.body}');
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Container(
-          // child: Center(child: Text('Hi',style: TextStyle(fontSize: 20),)),
-          child: Form(
-              key: _formKey,
-              child: Container(
-                width: 290,
-                color: Colors.grey,
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email_outlined),
-                    hintText: "Email",
-                    contentPadding: EdgeInsets.symmetric(vertical: 2.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                  ),
-                ),
-              )),
-        ),
-      );
+      print('Error: ${response.statusCode}');
+      print(jsonDecode(response.body));
+      return jsonDecode(response.body)['error'];
     }
   }
 
-  // Widget TakePassword(){
+  void _resetPassword() {
+    if (_formKey.currentState?.validate() ?? false) {
+      String email = emailController.text.trim();
+      takeEmailAPI(email);
+    }
+  }
 
-  // }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Form(
+                key: _formKey,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 290,
+                  color: Colors.grey,
+                  child: TextFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email cannot be empty';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email_outlined),
+                      hintText: "Email",
+                      contentPadding: EdgeInsets.symmetric(vertical: 2.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              child: ElevatedButton(
+                onPressed: _resetPassword,
+                child: Text('Reset Password'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
