22d360322cda4fa9185195a03a9d93b0ceec1df2
Euclid Stellar <euclidstellar@gmail.com>
2023-12-04 01:24:08 +0530

    check

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -55,7 +55,7 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   Color _generateRandomColor() {
-    return Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt())
         .withOpacity(1.0);
   }
 
